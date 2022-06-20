52a5afc07f219b48991dff9543b2c3e1adabb013
Muhammed-Shikhani <muhammed.shikhani@ufz.de>
2022-06-20 20:26:52 +0200

    1- script to extract surface and bottom temperature from the nc file 2- script to calculate anomlies and plit 3- modifcation to make met function to account for leap year by interploating the 29th of Feb. 4- adding the outcome of LER runs using FLake only

--- LER_Sevan_version2/function_make_met_file.R
+++ LER_Sevan_version2/function_make_met_file.R
@@ -1,6 +1,6 @@
 
 make_met_file <- function(list_name){
-  
+
   df_meteo <- data.frame('datetime' = paste0(c(my_dates,my_dates[length(my_dates)]+1), " 00:00:00"),
                          'Ten_Meter_Uwind_vector_meterPerSecond' = list_name$uas$Data,
                          'Ten_Meter_Vwind_vector_meterPerSecond' = list_name$vas$Data,
@@ -12,6 +12,7 @@ make_met_file <- function(list_name){
                          'Longwave_Radiation_Downwelling_wattPerMeterSquared' = list_name$rlds$Data,
                          'Cloud_Cover_decimalFraction' = list_name$clt$Data,
                          'Ten_Meter_Elevation_Wind_Speed_meterPerSecond' = list_name$sfcWind$Data)
-  
-  write.csv(df_meteo, "LakeEnsemblR_meteo_standard.csv", row.names = F, quote = F)
+  df_app <- zoo::na.approx(as.matrix(df_meteo[,-1]))
+  df_meteo[,-1] <- df_app
+ write.csv(df_meteo, "LakeEnsemblR_meteo_standard.csv", row.names = F, quote = F)
 }
